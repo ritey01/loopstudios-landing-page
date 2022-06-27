c11b9a1163fd5b8cc53734e3a50ff5c1af41f14b
Sally Wright <sallywright@developersinstitute.co.nz>
2022-06-27 14:26:21 +1200

    Add banner & creations

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../Banner";
+import Creations from "../Creations";
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <Creations />
+    </>
+  );
+};
+
+export default Home;
