65db1227fccebd3cfff5f2babd1dcf47a92b8ff7
Sally Wright <sallywright@developersinstitute.co.nz>
2022-06-27 14:26:06 +1200

    Add basic pages

---
+++ src/components/Pages/Careers.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Careers = () => {
+  return (
+    <>
+      <h1>Careers</h1>
+    </>
+  );
+};
+
+export default Careers;
---
+++ src/components/Pages/Events.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Events = () => {
+  return (
+    <>
+      <h1>Events</h1>
+    </>
+  );
+};
+
+export default Events;
