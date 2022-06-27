12c2c10a223df817d390f52d7af7d26a7fb2321d
Sally Wright <sallywright@developersinstitute.co.nz>
2022-06-27 14:24:42 +1200

    Add page import and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import Header from "./Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
+import Careers from "./components/Pages/Careers";
+import Events from "./components/Pages/Events";
+import Products from "./components/Pages/Products";
+import Support from "./components/Pages/Support";
 
 const App = () => {
-  return <Header />;
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/careers" element={<Careers />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/support" element={<Support />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 export default App;
